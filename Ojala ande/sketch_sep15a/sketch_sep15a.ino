bcca4fff4befac5f630f82471016fb7249e4145e
NachoBerardo <nachoberardo2005@gmail.com>
2022-09-15 14:43:55 -0300

    a

---
+++ Ojala ande/sketch_sep15a/sketch_sep15a.ino
@@ -0,0 +1,105 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <time.h>
+
+
+// Update these with values suitable for your network.
+const char* ssid = "Proyecto";
+const char* password = "latienedario";
+const char* mqtt_server = "8c50c13b35c240f18787982560b1eee7.s1.eu.hivemq.cloud";
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (500)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+
+void reconnect() {
+  // Loop until we’re reconnected
+  while (!client->connected()) {
+    Serial.print("Attempting MQTT connection…");
+    String clientId = "ESP8266Client - MyClient";
+    // Attempt to connect
+    // Insert your password
+    if (client.connect(clientId.c_str(), "inb2005", "SpotLight")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement…
+      client.publish("testTopic", "hello world");
+      // … and resubscribe
+      client.subscribe("testTopic");
+    } else {
+      Serial.print("failed, rc = ");
+      Serial.print(client->state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void setup() {
+  delay(500);
+  // When opening the Serial Monitor, select 9600 Baud
+  Serial.begin(9600);
+  delay(500);
+
+  setup_wifi();
+
+  WiFiClient ciente;
+  client = PubSubClient mqttClient(ciente); 
+
+  client.setServer(mqtt_server, 8883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client->connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client->publish("testTopic", msg);
+  }
+}
