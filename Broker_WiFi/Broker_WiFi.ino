cb44d91931f95b95c02e990950a1314350bbfb47
NachoBerardo <nachoberardo2005@gmail.com>
2022-10-06 14:57:16 -0300

    a

--- Broker_WiFi/Broker_WiFi.ino
+++ Broker_WiFi/Broker_WiFi.ino
@@ -1,14 +1,22 @@
-#include <WiFi.h>
-  #include <PubSubClient.h>
+  #include "Adafruit_MQTT.h"
+  #include "Adafruit_MQTT_Client.h"  
   #include <WiFiClientSecure.h>
-  #include <time.h>
+  #include <WiFi.h>
 
   //...........................................VARIABLES PARA WIFI...........................................
   const char* ssid = "Proyecto";
   const char* password = "latienedario";
   unsigned long previousMillis = 0;
   unsigned long interval = 30000;
+  
+  //...........................................VARIABLES PARA ADAFRUIT...........................................
+  #define AIO_SERVER      "io.adafruit.com" 
+  #define AIO_SERVERPORT  1883
+  #define AIO_USERNAME  "inb2005"
+  #define AIO_KEY       "aio_HBQJ41ZdTKg7XNxMa7VDMEVzazMa"
 
+  WiFiClient client;
+  
    void initWiFi()
   {
     WiFi.begin(ssid, password);
